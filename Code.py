2e3dc84071e194924aa22fdf2c2218e5ea1df9d0
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-10-05 11:56:10 +0800

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,16 @@
+import csv
+
+# Replace 'input.csv' with the path to your CSV file
+csv_file = 'input.csv'
+# Replace 'output.fasta' with the desired output FASTA file path
+fasta_file = 'output.fasta'
+
+# Open the CSV file for reading and the FASTA file for writing
+with open(csv_file, 'r') as csvfile, open(fasta_file, 'w') as fastafile:
+    csv_reader = csv.reader(csvfile)
+
+    for row in csv_reader:
+        if len(row) == 2:
+            name, sequence = row
+            # Write the FASTA entry to the FASTA file
+            fastafile.write(f'>{name}\n{sequence}\n')
